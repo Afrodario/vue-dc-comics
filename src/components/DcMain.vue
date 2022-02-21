d5e54295ace5bf149b5fef7bdf65db4ecc8394b8
Dario La Tegola <dariolategola@gmail.com>
2022-02-21 16:46:15 +0100

    Main completato

--- src/components/DcMain.vue
+++ src/components/DcMain.vue
@@ -1,10 +1,39 @@
 <template>
-    <main class="debug container">
-        <section class="debug">
-
+    <main class="debug">
+        <section class="main-top">
+          <div class="container">
+            <h3>Content Goes Here</h3>
+          </div>
         </section>
-        <section class="debug">
+        <section class="main-bottom flex">
+          <div class="container flex gallery">
+            <div class="card flex">
+              <img src="../assets/img/buy-comics-digital-comics.png">
+              <span>Digital Comics</span>
+            </div>
+            <div class="card flex">
+              <img src="../assets/img/buy-comics-merchandise.png">
+              <span>DC Merchandise</span>
+            </div>
+            <div class="card flex">
+              <img src="../assets/img/buy-comics-shop-locator.png">
+              <span>Subscription</span>
+            </div>
+            <div class="card flex">
+              <img src="../assets/img/buy-comics-subscriptions.png">
+              <span>Comic Shop Locator</span>
+            </div>
+            <div class="card flex">
+              <img src="../assets/img/buy-dc-power-visa.svg">
+              <span>DC Power Visa</span>
+            </div>
+            
 
+            <!-- <img src="../assets/img/buy-comics-merchandise.png">
+            <img src="../assets/img/buy-comics-shop-locator.png">
+            <img src="../assets/img/buy-comics-subscriptions.png">
+            <img src="../assets/img/buy-dc-power-visa.svg"> -->
+          </div>
         </section>
     </main>
 </template>
@@ -16,12 +45,30 @@ export default {
 </script>
 
 <style scoped lang="scss">
+@import "../assets/style/variables.scss";
   main {
-    height: 200px;
-    background-color: blue;
 
-      section {
-        height: 100px;
+      .main-top {
+        background-color: #1c1c1c;
+
+        h3 {
+          color: white;
+          text-align: center;
+          font-size: 30px;
+        }
+      }
+
+      .main-bottom {
+        background-color: $primaryColor;
+        height: 200px;
+
+        .gallery {
+          justify-content: space-between;
+        }
+
+        img {
+          width: 50px;
+        }
       }
   }
 </style>
