7b05466eff794c33402dad05720051da25af2bb8
Dario La Tegola <dariolategola@gmail.com>
2022-02-21 15:13:37 +0100

    Struttura iniziale

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <DcHeader/>
+    <DcMain/>
+    <DcFooter/>
+  </div>
+</template>
+
+<script>
+import DcHeader from './components/DcHeader.vue'
+import DcMain from './components/DcMain.vue'
+import DcFooter from './components/DcFooter.vue'
+
+export default {
+  name: 'App',
+  components: {
+    DcHeader,
+    DcMain,
+    DcFooter
+  }
+}
+</script>
+
+<style lang="scss">
+
+</style>
---
+++ src/components/DcFooter.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'DcFooter',
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
